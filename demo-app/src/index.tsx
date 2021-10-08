d5c0d381d54573474b056f1e1be91970ec92409c
Max Caber <mcaber@cta.org>
2021-10-08 10:33:49 -0700

    HOC Demo more changes

--- demo-app/src/index.tsx
+++ demo-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FromOldToNewReact from './FromOldToNewReact';
+import HocDemo from './hoc/HocDemo';
 import './index.css';
 import MUIGridDemo from './mui-demos/MUIGridDemo';
 import App from './shopping-cart/redux/app';
@@ -8,7 +9,7 @@ import App from './shopping-cart/redux/app';
 // import StyledComponentsTheme from './mui-demos/StyledComponentsTheme';
 
 ReactDOM.render(
-  <MUIGridDemo />
+  <HocDemo />
   ,
   document.getElementById('root')
 );
